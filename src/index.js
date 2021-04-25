804157ce102de668dfacb1cfdcaffc8b143b9cd2
Steffen Kötte <steffen.koette@gmail.com>
2021-04-24 17:47:18 -0700

    Do API call to read PR info

--- src/index.js
+++ src/index.js
@@ -9,9 +9,6 @@ async function main() {
 	const token = core.getInput("github-token")
 	const lcovFile = core.getInput("lcov-file") || "./coverage/lcov.info"
 	const baseFile = core.getInput("lcov-base")
-	const commit = core.getInput("commit")
-	const head = core.getInput("head")
-	const base = core.getInput("base")
 	const pr_number = core.getInput("pr_number")
 	
 	const raw = await fs.readFile(lcovFile, "utf-8").catch(err => null)
@@ -24,13 +21,19 @@ async function main() {
 	if (baseFile && !baseRaw) {
 		console.log(`No coverage report found at '${baseFile}', ignoring...`)
 	}
+	
+	const pull_request = await new GitHub(token).pulls.get({
+		 owner: context.repo.owner,
+		 repo: context.repo.repo,
+		 pull_number: pr_number,
+	});
 
 	const options = {
 		repository: context.payload.repository.full_name,
 		prefix: `${process.env.GITHUB_WORKSPACE}/`,
-		commit: commit,
-		head: head,
-		base: base
+		commit = pull_request.head.sha
+		head = pull_request.head.ref
+		base = pull_request.base.ref
 	}
 
 	const lcov = await parse(raw)
